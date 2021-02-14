bc48e7e9eadac7bd2add84aaf5070a939a8cb5d7
Craig Bovis <craig@bovis.me.uk>
2021-02-14 11:41:38 +0100

    Update constants

--- src/constants.js
+++ src/constants.js
@@ -1,28 +1,28 @@
 const QUEUE = {
   ADDRESS_PROCESSING: 'address-processing',
-  BULK_INDEXING: 'bulk-indexing',
+  EVENT_PROCESSING: 'event-processing',
   FILL_INDEXING: 'fill-indexing',
   FILL_PROCESSING: 'fill-processing',
   INDEXING: 'indexing',
   TOKEN_PROCESSING: 'token-processing',
-  TRADED_TOKEN_INDEXING: 'traded-token-indexing',
   TRANSACTION_PROCESSING: 'transaction-processing',
+  TRADED_TOKEN_INDEXING: 'traded-token-indexing',
 };
 
 const JOB = {
-  BULK_INDEX_FILLS: 'bulk-index-fills',
-  BULK_INDEX_TRADED_TOKENS: 'bulk-index-traded-tokens',
   BULK_UPDATE_TOKEN_METADATA: 'bulk-update-token-metadata',
   CONVERT_PROTOCOL_FEE: 'convert-protocol-fee',
+  CONVERT_RELAYER_FEES: 'convert-relayer-fees',
+  CREATE_FILLS_FOR_EVENT: 'create-fills-for-event',
+  CREATE_TOKEN: 'create-token',
   FETCH_ADDRESS_TYPE: 'fetch-address-type',
-  FETCH_FILL_STATUS: 'fetch-fill-status',
   FETCH_TOKEN_METADATA: 'fetch-token-metadata',
+  FETCH_TRANSACTION: 'fetch-transaction',
   INDEX_FILL: 'index-fill',
   INDEX_FILL_PROTOCOL_FEE: 'index-fill-protocol-fee',
-  INDEX_FILL_STATUS: 'index-fill-status',
+  INDEX_FILL_TRADERS: 'index-fill-traders',
   INDEX_FILL_VALUE: 'index-fill-value',
   INDEX_TRADED_TOKENS: 'index-traded-tokens',
-  SCHEDULE_TRADED_TOKENS_BULK_INDEXING: 'schedule-traded-tokens-bulk-indexing',
 };
 
 module.exports = { JOB, QUEUE };
