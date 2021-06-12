7e8d80739dff915b4d574962918ea58d202b64d6
Craig Bovis <craig@bovis.me.uk>
2021-06-12 18:01:09 +0200

    Remove old queues

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,10 @@
 const QUEUE = {
   ADDRESS_PROCESSING: 'address-processing',
   EVENT_PROCESSING: 'event-processing',
-  FILL_INDEXING: 'fill-indexing',
   FILL_PROCESSING: 'fill-processing',
   INDEXING: 'indexing',
   TOKEN_PROCESSING: 'token-processing',
   TRANSACTION_PROCESSING: 'transaction-processing',
-  TRADED_TOKEN_INDEXING: 'traded-token-indexing',
 };
 
 module.exports = { QUEUE };
