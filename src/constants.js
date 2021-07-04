a1eedeb4edfae1ec308e6223dc2343eeb0245ce9
Craig Bovis <craig@bovis.me.uk>
2021-07-04 20:11:44 +0200

    Update queues definition

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,8 @@
 const QUEUE = {
-  ADDRESS_PROCESSING: 'address-processing',
-  EVENT_PROCESSING: 'event-processing',
-  FILL_PROCESSING: 'fill-processing',
+  DEFAULT: 'default',
+  ETH_DATA: 'eth-data',
   INDEXING: 'indexing',
   PRICING: 'pricing',
-  TOKEN_PROCESSING: 'token-processing',
-  TRANSACTION_PROCESSING: 'transaction-processing',
 };
 
 module.exports = { QUEUE };
